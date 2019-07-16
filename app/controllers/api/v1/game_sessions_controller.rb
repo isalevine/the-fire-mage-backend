217671291f0ac82143f339337f5db4bb50d0ecc4
isalevine <isa.loves.coding@gmail.com>
2019-07-15 21:30:33 -0700

    building in database-cleaner, based on game_session being in_progress or complete

--- app/controllers/api/v1/game_sessions_controller.rb
+++ app/controllers/api/v1/game_sessions_controller.rb
@@ -27,6 +27,9 @@ class Api::V1::GameSessionsController < ApplicationController
 
 
   def destroy
+    find_game_session
+    @game_session.destroy
+    render json: {status: "GameSession deleted!"}
   end
 
 
