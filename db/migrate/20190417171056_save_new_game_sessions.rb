9e93119c0f3e0b085014a0d40c067111fbc31fe2
isalevine <isa.loves.coding@gmail.com>
2019-07-29 14:36:16 -0700

    fixed migration name

--- db/migrate/20190417171056_save_new_game_sessions.rb
+++ db/migrate/20190417171056_save_new_game_sessions.rb
@@ -1,4 +1,4 @@
-class saveNewGameSessions < ActiveRecord::Migration[5.2]
+class SaveNewGameSessions < ActiveRecord::Migration[5.2]
   def change
     create_table :game_sessions do |t|
       t.boolean :in_progress, null: false  #consider adding default: true
