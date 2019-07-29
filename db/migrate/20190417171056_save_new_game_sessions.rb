13289070b839b8f5d01e5a9bbc4b45fe29330406
isalevine <isa.loves.coding@gmail.com>
2019-07-29 15:13:14 -0700

    tweaked first db:migrate to set defaults on null:false params--MAY CAUSE LOCAL ISSUES, test asapgit add .

--- db/migrate/20190417171056_save_new_game_sessions.rb
+++ db/migrate/20190417171056_save_new_game_sessions.rb
@@ -1,8 +1,8 @@
 class SaveNewGameSessions < ActiveRecord::Migration[5.2]
   def change
     create_table :game_sessions do |t|
-      t.boolean :in_progress, null: false  #consider adding default: true
-      t.boolean :complete, null: false  #consider adding default: false
+      t.boolean :in_progress, null: false, default: true  #consider adding default: true
+      t.boolean :complete, null: false, default: false  #consider adding default: false
       t.string :expiration_date  #better to set here, or with Javascript?
 
       t.timestamps
